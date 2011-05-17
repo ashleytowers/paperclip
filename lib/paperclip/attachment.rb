ac7f374b81dc51878dfbfe9ffc63e425fb063f87
Ashley Towers <a.towers@sheffield.ac.uk>
2011-05-17 15:13:08 +0100

    Added option to keep old files

--- lib/paperclip/attachment.rb
+++ lib/paperclip/attachment.rb
@@ -359,9 +359,11 @@ module Paperclip
 
     def queue_existing_for_delete #:nodoc:
       return unless file?
-      @queued_for_delete += [:original, *styles.keys].uniq.map do |style|
-        path(style) if exists?(style)
-      end.compact
+      unless @options[:keep_old_files]
+        @queued_for_delete += [:original, *styles.keys].uniq.map do |style|
+          path(style) if exists?(style)
+        end.compact
+      end
       instance_write(:file_name, nil)
       instance_write(:content_type, nil)
       instance_write(:file_size, nil)
